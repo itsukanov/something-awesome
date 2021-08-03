0ce2c2cf85a58772f8d23d253f3fe2da6859df4f
Ivan Tsukanov <itsukanov@deloitte.com>
2021-08-03 09:51:41 +0700

    Deleted unused property from Config

--- base-app/src/main/scala/com/itsukanov/common/restapi/Config.scala
+++ base-app/src/main/scala/com/itsukanov/common/restapi/Config.scala
@@ -9,7 +9,6 @@ object Config {
   val entryPoint            = ServerConfig(localHost, 8080)
   val companyInfo           = ServerConfig(localHost, 8081)
   val companyPrices         = ServerConfig(localHost, 8082)
-  val externalCompanyPrices = ServerConfig(localHost, 8082)
 
   val jaeger = ServerConfig(localHost, 6831)
 }
